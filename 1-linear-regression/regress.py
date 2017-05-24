3af7c38f033aac3387a758bf10f2ecb1ceab989e
Alex <asavchenkov1@gmail.com>
2017-05-23 20:30:33 -0700

    added data for hw1, as well as starter code

--- 1-linear-regression/regress.py
+++ 1-linear-regression/regress.py
@@ -1,1 +1,10 @@
 import numpy as np
+import matplotlib.pyplot as plt
+
+#returns a numpy array
+def load_data():
+    return np.loadtxt('../data/ex1data1.txt', delimiter = ',')
+    
+if __name__ == '__main__':
+    
+    data = load_data()
